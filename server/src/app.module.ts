2032666e0c623817db57fbfa8050d463226a98e9
mohammedafsalk <techafs456@gmail.com>
2023-12-10 22:31:00 +0530

    changes made and packages added in server side

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB),
+  ],
+  controllers: [],
+  providers: [UserService, AuthService],
 })
 export class AppModule {}
